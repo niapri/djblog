b50922e3f668a6f823bcc541bfea59d726f98906
niapri <wills.brandy@gmail.com>
2017-02-12 17:44:54 -0600

    Blank

--- djblog/settings.py
+++ djblog/settings.py
@@ -87,8 +87,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'wills.brandy@gmail.com'
-EMAIL_HOST_PASSWORD = 'Drummajor#1'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 # Password validation
